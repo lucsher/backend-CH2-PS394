e28670b665e1e595e22a5c90ffa1195e6f39e8bb
lucsher <21082010055@student.upnjatim.ac.id>
2023-12-05 15:15:34 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import {getUsers, Register, Login, Logout} from "../controller/users.js"
 import { verifyToken } from "../middleware/verifytoken.js";
 import { refreshToken } from "../controller/refreshtoken.js";
+import { getAllCities, getAllCategories, getCity, getCategory, getPlaceName, getAllPlace } from "../controller/data.js";
 
 const router = express.Router();
 
@@ -11,4 +12,10 @@ router.post('/login',Login);
 router.get('/token',refreshToken);
 router.delete('/logout',Logout);
 
+router.get('/cities', getAllCities)
+router.get('/categories', getAllCategories)
+router.get('/cities/:city', getCity)
+router.get('/categories/:category', getCategory)
+router.get('/place', getAllPlace)
+router.get('/place/:name', getPlaceName)
 export default router;
