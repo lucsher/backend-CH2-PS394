4064f301878661c3c45a120146eadde41f6cc670
Luky <117432561+Conniheim@users.noreply.github.com>
2023-12-15 21:55:04 +0700

    Update db.js add from cloudsql

--- config/db.js
+++ config/db.js
@@ -1,9 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('setravel_db', 'admin_project', '',{
-    host: "localhost",
-    dialect: "mysql"
+const db = new Sequelize({
+   dialect: 'mysql', // Change this to 'postgres' if using PostgreSQL
+    username: 'izzul',
+    password: 'izzul',
+    database: 'setravel_db',
+    host: '34.101.149.181', // Replace with your Cloud SQL instance IP
+    port: '3306', // Replace with your Cloud SQL instance port
+    socketPath: '/cloudsql/capstone-setravel:asia-southeast2:setravel',
+    logging: false // Set to true to log SQL queries (optional)
 });
+
+//const db = new Sequelize('setravel_db', 'admin_project', '',{
+//    host: "localhost",
+//    dialect: "mysql"
+//});
 
 // const db = new Sequelize('citiesdb', 'root', '',{
 //     host: "localhost",
