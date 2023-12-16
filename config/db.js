65e4a0695fa982cd90beb6b878e58f410021db30
Luky <117432561+Conniheim@users.noreply.github.com>
2023-12-16 08:48:46 +0700

    Update db.js hide credentials

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize({
    dialect: 'mysql', // Change this to 'postgres' if using PostgreSQL
-    username: 'izzul',
-    password: 'izzul',
-    database: 'setravel_db',
-    host: '34.101.149.181', // Replace with your Cloud SQL instance IP
+    username: '<username>',
+    password: '<password>',
+    database: '<database>',
+    host: '<host>', // Replace with your Cloud SQL instance IP
     port: '3306', // Replace with your Cloud SQL instance port
     socketPath: '/cloudsql/capstone-setravel:asia-southeast2:setravel',
     logging: false // Set to true to log SQL queries (optional)
