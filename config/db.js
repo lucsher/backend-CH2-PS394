32517ff3ced3891ebb8c70de208dfa98ed83af07
Luky <117432561+Conniheim@users.noreply.github.com>
2023-12-19 09:24:25 +0700

    Update db.js
    Add .env variable

--- config/db.js
+++ config/db.js
@@ -1,25 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 
 const db = new Sequelize({
    dialect: 'mysql', // Change this to 'postgres' if using PostgreSQL
-    username: '<username>',
-    password: '<password>',
-    database: '<database>',
-    host: '<host>', // Replace with your Cloud SQL instance IP
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST, // Replace with your Cloud SQL instance IP
     port: '3306', // Replace with your Cloud SQL instance port
     socketPath: '/cloudsql/capstone-setravel:asia-southeast2:setravel',
     logging: false // Set to true to log SQL queries (optional)
 });
-
-//const db = new Sequelize('setravel_db', 'admin_project', '',{
-//    host: "localhost",
-//    dialect: "mysql"
-//});
-
-// const db = new Sequelize('citiesdb', 'root', '',{
-//     host: "localhost",
-//     dialect: "mysql"
-// });
-
-
-export default db;
