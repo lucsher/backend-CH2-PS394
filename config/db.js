87d3c02990ae837faa3b06869b1f5159729e4600
Hanif <21082010055@student.upnjatim.ac.id>
2023-12-21 21:05:49 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,12 +4,12 @@ dotenv.config()
 
 const db = new Sequelize({
    dialect: 'mysql', 
-    username: 'izzul',
-    password: 'izzul',
-    database: 'setravel_db',
-    host: '34.101.149.181', 
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST, 
     port: '3306', 
-    socketPath: '/cloudsql/capstone-setravel:asia-southeast2:setravel',
+    socketPath: process.env.SOCKET,
     logging: false 
 });
 
