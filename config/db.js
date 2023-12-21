ba36984dbc81895364866d294c9dee01f56466c4
Hanif <21082010055@student.upnjatim.ac.id>
2023-12-21 21:12:54 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,8 +6,8 @@ const db = new Sequelize({
    dialect: 'mysql', 
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    host: process.env.HOST, 
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST, 
     port: '3306', 
     socketPath: process.env.SOCKET,
     logging: false 
