a49f41818af3d23980b8f6a5b7c07d098feb08be
Luky <117432561+Conniheim@users.noreply.github.com>
2023-12-21 21:00:58 +0700

    Update verifytoken.js

--- middleware/verifytoken.js
+++ middleware/verifytoken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) =>{
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token==null) return res.sendStatus(401);
-    jwt.verify(token, sup02i2bnqs9zmxulin0o3mshky8tujhude0k3bdrmxuv3bg3s3pqikdyof1v197, (err, decoded) =>{
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
         if(err) return res.sendStatus(403);
         req.email = decoded.email;
         next();
